54bf30169663e0d287b05288135c6fbc5e280222
Steve Yeom <krazyeom@gmail.com>
2013-06-27 08:15:51 +0900

    Initial Commit

---
+++ DragonRider/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  DragonRider
+//
+//  Created by Steve Yeom on 6/27/13.
+//  Copyright Appilogue 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
